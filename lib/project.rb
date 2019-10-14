14d98c06f3eb98b8d9003d9e9808ca1fadbd40fb
Jared S <jaredjames2020@gmail.com>
2019-10-14 14:07:29 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,9 @@
 class Project
 
-<<<<<<< HEAD
+
   attr_reader :title, :backers, :back_project
-=======
   attr_reader :title, :backers
->>>>>>> 57bd71058328fb9fc73efc590b5f6ea9935ac25f
+
 
   def initialize(title)
     @title = title
@@ -12,18 +11,14 @@ class Project
   end
   
   def add_backer(backer)
-<<<<<<< HEAD
+
     @backers << backer 
     if !backer.backed_projects.include?(self)
       backer.back_project(self)
     end
   end
-=======
-    @backers << backer
-    backer.back_project(self)
+   
   end
   
-  
->>>>>>> 57bd71058328fb9fc73efc590b5f6ea9935ac25f
 
 end
